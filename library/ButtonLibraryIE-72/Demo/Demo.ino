dc40b567aace84db58ea22309d02ae550a8b406f
emil817 <75312001+emil817@users.noreply.github.com>
2020-12-13 09:51:30 +0700

    Update Demo.ino

--- library/ButtonLibraryIE-72/Demo/Demo.ino
+++ library/ButtonLibraryIE-72/Demo/Demo.ino
@@ -1,11 +1,20 @@
 #include <ButtonLibraryIE-72.h>
 
-IE_72_Button Button(8,1);
+IE_72_Button Button(38,PULLDOWN);
+
 void setup() {
-  Button.getState();
+  Serial.begin(11520);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static uint16_t buttonCount = 0;
+  switch (Button.getState())
+  {
+    case 0: break;
+    case 1: Serial.println("Button pressed " + String(++buttonCount) + " times."); break;
+    case 2: Serial.println("Button holded."); break;
+    case 3: Serial.println("Hold repeated."); break;
+    case 4: Serial.println("Button released."); break;
+  }
 
 }

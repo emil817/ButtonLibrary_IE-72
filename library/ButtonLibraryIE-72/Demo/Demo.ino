2fec3fcf51aa11fef5196dce6057e6bd86b088e5
emil817 <75312001+emil817@users.noreply.github.com>
2020-12-15 21:20:50 +0700

    Revert "Update Demo.ino"
    This reverts commit dc40b567aace84db58ea22309d02ae550a8b406f.

--- library/ButtonLibraryIE-72/Demo/Demo.ino
+++ library/ButtonLibraryIE-72/Demo/Demo.ino
@@ -1,20 +1,11 @@
 #include <ButtonLibraryIE-72.h>
 
-IE_72_Button Button(38,PULLDOWN);
-
+IE_72_Button Button(8,1);
 void setup() {
-  Serial.begin(11520);
+  Button.getState();
 }
 
 void loop() {
-  static uint16_t buttonCount = 0;
-  switch (Button.getState())
-  {
-    case 0: break;
-    case 1: Serial.println("Button pressed " + String(++buttonCount) + " times."); break;
-    case 2: Serial.println("Button holded."); break;
-    case 3: Serial.println("Hold repeated."); break;
-    case 4: Serial.println("Button released."); break;
-  }
+  // put your main code here, to run repeatedly:
 
 }
